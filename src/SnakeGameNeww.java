426262c9c23e473d1c26bd4b4dd4e7016bebf7dc
venyyan <venyyan@users.noreply.github.com>
2019-01-22 19:43:48 +0200

    created JFrame

--- src/SnakeGameNeww.java
+++ src/SnakeGameNeww.java
@@ -1,4 +1,9 @@
-public class SnakeGameNeww {
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class SnakeGameNeww extends JFrame {
+
     static char dir = '•';
     public static int[] map = new int[] {50, 16};
     public static int[] tailX, tailY;
@@ -6,4 +11,13 @@ public class SnakeGameNeww {
     public static int[] food = new int[2];
     public static int snakeLength = 0;
     public static boolean gameOver = false;
+
+    public SnakeGameNeww() {
+
+        this.setSize(250, 150);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Snake");
+        this.setVisible(true);
+
+    }
 }
